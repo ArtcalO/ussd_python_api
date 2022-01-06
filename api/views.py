28e928bcb5cae51aaa1ac231ceca052a64a80155
ArtcalO <niyca14@gmail.com>
2022-01-06 23:34:00 +0100

    test con error

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse
 
 @api_view(['POST'])
 def uss_callback(request):
@@ -8,14 +9,11 @@ def uss_callback(request):
     serviceCode = request.data.get("serviceCode", None)
     phoneNumber = request.data.get("phoneNumber", None)
     text = request.data.get("text", None)
-    menu_text=""
 
     #serve menus based on text
     if text == "":
-        menu_text = "CON What would you want to check \n"
-        menu_text += "1. My Account \n"
-        menu_text += "2. My phone number \n"
-        menu_text += "3. My branch"
+
+        menu_text = "CON "+"What would you want to check \n"
 
     elif text =="1":
         menu_text = "CON Choose the account information that you want to view \n"
@@ -30,4 +28,5 @@ def uss_callback(request):
 
     elif text =="1*2":
             menu_text = "END Your BALANCE  is KES 120/-"
-    return Response(menu_text,200)
+
+    return Response(menu_text, 200)
