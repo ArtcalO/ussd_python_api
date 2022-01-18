177c22daf204867086824ecbea4d8f214e5f2bc2
ArtcalO <niyca14@gmail.com>
2022-01-18 13:30:28 +0100

    new file

---
+++ api/ussdpoo.py
@@ -0,0 +1,101 @@
+montant = 0
+contenu = ""
+
+def menu_niveau1():
+    menu_text = "Entrer le numero de telephone: \n"
+    menu_text += "0. Retour"
+    return menu_text
+
+def menu_niveau2():
+    menu_text = "Entrer le montant: \n"
+    menu_text += "0. Retour"
+    return menu_text
+
+def menu_niveau3():
+    menu_text = "Entrer le contenu du transfert: \n"
+    menu_text += "0. Retour"
+    return menu_text
+
+def menu_niveau4():
+    menu_text = "Entrer le code PIN: \n"
+    menu_text += "0. Retour"
+    return menu_text
+
+def menu_niveau5():
+    menu_text = f"Confirmation de transfert {montant} \n \
+                vers Niyungeko Carmel, 68 46 18 41 pour \n \
+                {contenu}, frais: 450 Fbu. Choisissez : \n"
+    menu_text += "1.Oui"
+    menu_text += "2.Non"
+    return menu_text
+
+def menu_lumicash():
+    menu_text = "Lumicash- Choisissez \n"
+    menu_text += "1. Transfert\n"
+    menu_text += "2. Retirer \n"
+    menu_text += "3. Achat des unités \n"
+    menu_text += "4. Payer les factures \n"
+    menu_text += "5. Payer le marchand \n"
+    menu_text += "6. Service bancaires \n"
+    menu_text += "7. OBR \n"
+    menu_text += "8. Mairie \n"
+    menu_text += "9. Engrais \n"
+    menu_text += "10. Service \n"
+
+    return menu_text
+
+def handler_menu_lumicash():
+    print(menu_lumicash())
+    text = input("")
+    if text == '1':
+        handler_niveau1()
+def handler_niveau1():
+    print(menu_niveau1())
+    text=input("")
+    if text!='0':
+        handler_niveau2()
+    elif text == '0':
+        handler_menu_lumicash()
+
+def handler_niveau2():
+    print(menu_niveau2())
+    text=input("")
+    if text!='0':
+        montant=text
+        handler_niveau3()
+    elif text == '0':
+        handler_menu_lumicash()
+
+def handler_niveau3():
+    print(menu_niveau3())
+    text=input("")
+    if text!='0':
+        contenu=text
+        handler_niveau4()
+    elif text == '0':
+        handler_menu_lumicash()
+
+def handler_niveau4():
+    print(menu_niveau4())
+    text=input("")
+    if text!='0':
+        handler_niveau5()
+    elif text == '0':
+        handler_menu_lumicash()
+
+def handler_niveau5():
+    print(menu_niveau5())
+    text=input("")
+    if text =='1':
+        print("Transfert avec success !")
+    elif text == '2':
+        print("Echec de transfert !")
+
+def main():
+    text = input("Entrer le code ussd :")
+    if(text == "*163#"):
+        handler_menu_lumicash()
+    else:
+        print("code invalide")
+
+main()
